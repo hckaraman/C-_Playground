050c21a7b0a62c5277b37ef154363641eabef9d1
Cagri <cagrkaraman@gmail.com>
2020-09-29 09:09:42 +0300

    Read SQLite

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Diagnostics;
 using System.Threading;
+using System.Data.SQLite;
+using Playground;
+
 
 // using .Interop.TimeSeries;
 // using Microsoft.Data.Analysis;
@@ -32,7 +35,7 @@ namespace Test
 
             try
             {
-                Console.WriteLine(data[0, 2]);
+                // Console.WriteLine(data[0, 2]);
             }
             catch (Exception e)
             {
@@ -47,7 +50,7 @@ namespace Test
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
                 ts.Hours, ts.Minutes, ts.Seconds,
                 ts.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
+            // Console.WriteLine("RunTime " + elapsedTime);
 
             // DateTime start = new DateTime();
             DateTime currentTime = DateTime.Now;
@@ -96,7 +99,62 @@ namespace Test
                 {"at", 41}
             };
 
-            myList.ForEach(p => Console.WriteLine("{0}", p));
+            // myList.ForEach(p => Console.WriteLine("{0}", p));
+            var selected = myList.Select(p => p == 5);
+
+            // foreach (var b in selected)
+            // {
+            //     Console.WriteLine(b);
+            // }
+
+            List<Todo> todo = new List<Todo>()
+            {
+                new Todo {Description = "Task 1", Hours = 15, status = Status.Compleated},
+                new Todo {Description = "Task 2", Hours = 12, status = Status.Deleted},
+                new Todo {Description = "Task 3", Hours = 5, status = Status.InProgress},
+                new Todo {Description = "Task 4", Hours = 3, status = Status.NotStarted},
+                new Todo {Description = "Task 5", Hours = 12, status = Status.Compleated},
+                new Todo {Description = "Task 6", Hours = 1, status = Status.OnHold},
+                new Todo {Description = "Task 7", Hours = 9, status = Status.NotStarted},
+            };
+
+            var newselect = todo.Where(p => p.Hours > 12);
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            foreach (var item in todo)
+            {
+                // Console.WriteLine(item.Description);
+            }
+
+            foreach (var b in newselect)
+            {
+                // Console.WriteLine(b.Description);
+            }
+
+            // todo.ForEach(p => Console.WriteLine("{0},{1:C},{2:F}", p.Description, p.Hours, p.status));
+
+
+            string cs = @"URI=file:E:\MGM\data.db";
+
+            using var con = new SQLiteConnection(cs);
+            con.Open();
+
+
+            int stationNo = 17018;
+            string stm = string.Format("SELECT * FROM temp t where t.ist_no = {0} LIMIT 10", stationNo);
+
+            using var cmd = new SQLiteCommand(stm, con);
+            using SQLiteDataReader rdr = cmd.ExecuteReader();
+
+
+            while (rdr.Read())
+            {
+                // var stationId = rdr.GetDecimal(0);
+                Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", rdr.GetDecimal(0), rdr.GetString(1), rdr.GetDecimal(2),
+                    rdr.GetDecimal(3), rdr.GetDecimal(4), rdr.GetDecimal(5), rdr.GetString(6));
+            }
         }
 
 
---
+++ Playground/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground
+{
+    class Todo
+    {
+        public String Description { get; set; }
+        public int Hours { get; set; }
+        public Status status { get; set; }
+    }
+
+    enum Status
+    {
+        NotStarted,
+        InProgress,
+        OnHold,
+        Compleated,
+        Deleted
+    }
+}
